6e8da68a7a3f77687f8a3208e08d993f6e8f2a23
madeval <alvarez.fing@gmail.com>
2025-12-17 10:59:42 -0300

    Fix jobs-listings selector and optimize DOM rendering with DocumentFragment

--- 02-ejercicio-javascript/fetch-data.js
+++ 02-ejercicio-javascript/fetch-data.js
@@ -15,8 +15,17 @@ fetch("./data.json")
     return ofertasDeTrabajo;
   })
 
-  .then((ofertasDeTrabajo) => {
-    const elementoUl = document.querySelector(".jobs-listing");
+.then((ofertasDeTrabajo) => {
+    // Faltó la `s` en `jobs-listings`. Por eso no cargaban los datos 
+    const elementoUl = document.querySelector(".jobs-listings");
+
+    /* 
+    createDocumentFragment() lo que hace es crear un contenedor en memoria que sirve para almacenar todos los elementos del DOM que queremos pintar.
+    Para que sirve esto? Para evitar re dibujar el HTML cada vez que insertamos un elemento dentro del forEach. Lo que hacemos es: agregamos los elementos en el contenedor virtual, y una vez estén todos, pintamos de una sola vez lo que hay en el contenedor sobre el DOM.
+    Esto mejora bastante el rendimiento, sobre todo cuando tenemos muchos elementos :)
+    */
+    const documentFragment = document.createDocumentFragment()
+
     ofertasDeTrabajo.forEach((oferta) => {
       const elementoLi = document.createElement("li");
       const article = document.createElement("article");
@@ -35,8 +44,9 @@ fetch("./data.json")
         <button class="button-apply-job">Aplicar</button> 
     `;
     elementoLi.appendChild(article);
-    elementoUl.appendChild(elementoLi);
+    documentFragment.appendChild(elementoLi);
     });
+    elementoUl.appendChild(documentFragment);
   })
   .catch((error) => {
     console.log("Error al mostrar los datos: ", error);
